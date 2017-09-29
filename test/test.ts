837afe68864ecc4dbbab46f28d38638ad2a46f29
wzy <twoeo@qq.com>
2017-09-29 14:41:58 +0800

    pub 0.2.0

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
-import RpcServer from './src/RpcServer';
-import PtlHelloWorld from './test/protocol/PtlHelloWorld';
-import ApiHelloWorld from './test/api/ApiHelloWorld';
+import RpcServer from '../src/RpcServer';
+import PtlHelloWorld from '../test/protocol/PtlHelloWorld';
+import ApiHelloWorld from '../test/api/ApiHelloWorld';
 
 let server = new RpcServer({
     protocolPath: path.resolve(__dirname, 'test/protocol'),
