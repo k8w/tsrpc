5339e9f5e353cbd8f43fd1203a644432880e367e
k8w <me@k8w.io>
2019-07-17 17:00:18 +0800

    try ws server

--- try/server/ws.ts
+++ try/server/ws.ts
@@ -1,71 +1,9 @@
-import { serviceProto, ServiceType } from '../proto/serviceProto';
+import { serviceProto } from '../proto/serviceProto';
 import * as path from "path";
+import { TsrpcServerWs } from '../../index';
 
-let server = new TSRPCServer<ServiceType & { session: any }>({
-    wsPort: 3000,
-    httpPort: 3001,
-    proto: serviceProto,
-    apiPath: path.resolve(__dirname, 'api'),
-
-    onServerWillStop: conns => new Promise(rs => {
-        console.log('onServerWillStop');
-        conns.forEach(v => { console.log(v.connId) });
-
-        setTimeout(() => {
-            console.log('onServerWillStop 2');
-            conns.forEach(v => { v.close() });
-        }, 2000)
-
-        setTimeout(rs, 8000);
-    })
+let server = new TsrpcServerWs({
+    proto: serviceProto
 });
-
-// server.implementApi('Test', call => {
-//     if (Math.random() > 0.75) {
-//         call.succ({
-//             reply: 'Hello, ' + call.data.name
-//         })
-//     }
-//     else if (Math.random() > 0.5) {
-//         call.error('What the fuck??', { msg: '哈哈哈哈' })
-//     }
-//     else if (Math.random() > 0.25) {
-//         throw new Error('这应该是InternalERROR')
-//     }
-//     else {
-//         throw new TSRPCError('返回到前台的错误', 'ErrInfo');
-//     }
-// });
-
-server.listenMsg('Chat', call => {
-    if (Math.random() > 0.5) {
-        call.conn.sendMsg('Chat', {
-            channel: call.data.channel,
-            userName: 'Reply',
-            content: 'R-' + call.data.content,
-            time: Date.now()
-        })
-    }
-    else {
-
-    }
-})
-
+server.autoImplementApi(path.resolve(__dirname, 'api'));
 server.start();
-
-// setInterval(() => {
-//     server.sendMsg(['1', '2'], 'Chat', {
-//         channel: 123,
-//         userName: 'System',
-//         content: 'Lalala',
-//         time: Date.now()
-//     }).catch(e => { })
-// }, 1000)
-
-// 优雅的停止
-// setTimeout(async () => {
-//     console.time('stop')
-//     await server.stop();
-//     console.timeEnd('stop');
-//     process.exit();
-// }, 3000);
