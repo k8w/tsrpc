1f9e4305e47eb4c097b73015a5681dc76bd8f663
k8w <me@k8w.io>
2021-03-26 16:18:13 +0800

    ConsoleColorLogger -> class

--- src/server/models/_ConsoleColorLogger.ts
+++ src/server/models/_ConsoleColorLogger.ts
@@ -1,17 +1,23 @@
+import 'colors';
 import { Logger } from "tsrpc-proto";
 
 let pid = process.pid.toString(16);
-export const ConsoleColorLogger: Logger = {
+export class ConsoleColorLogger implements Logger {
+
     debug(...args: any[]) {
         console.debug.call(console, `<${pid}> ${new Date().format()}`.gray, '[DEBUG]'.cyan, ...args);
-    },
+    }
+
     log(...args: any[]) {
         console.log.call(console, `<${pid}> ${new Date().format()}`.gray, '[INFO]'.green, ...args);
-    },
+    }
+
     warn(...args: any[]) {
         console.warn.call(console, `<${pid}> ${new Date().format()}`.gray, '[WARN]'.yellow, ...args);
-    },
+    }
+
     error(...args: any[]) {
         console.error.call(console, `<${pid}> ${new Date().format()}`.gray, '[ERROR]'.red, ...args);
-    },
+    }
+
 }
