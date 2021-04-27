64643afdfc7ad5de63bb905df9c2e8ad12cf20f3
King Wang <me@k8w.io>
2021-04-28 00:07:01 +0800

    rm docs (temporarily)

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import 'k8w-extend-native';
+// Common
 export { Logger, TsrpcError } from 'tsrpc-proto';
-export { HttpClient } from './client/http/HttpClient';
+export { HttpClient, HttpClientOptions } from './client/http/HttpClient';
 export { ApiCall } from './server/base/ApiCall';
+// Base
 export { BaseCall } from './server/base/BaseCall';
 export { BaseConnection, BaseConnectionOptions } from './server/base/BaseConnection';
 export { ApiHandler, BaseServer, MsgHandler } from './server/base/BaseServer';
 export { MsgCall } from './server/base/MsgCall';
 export { ApiCallHttp } from './server/http/ApiCallHttp';
+// Http
 export { HttpConnection, HttpConnectionOptions } from './server/http/HttpConnection';
 export { HttpServer, HttpServerOptions } from './server/http/HttpServer';
 export { MsgCallHttp } from './server/http/MsgCallHttp';
 export { PrefixLogger } from './server/models/PrefixLogger';
 export { TerminalColorLogger, TerminalColorLoggerOptions } from './server/models/TerminalColorLogger';
+// WebSocket
 export { ApiCallWs } from './server/ws/ApiCallWs';
 export { MsgCallWs } from './server/ws/MsgCallWs';
 export { WsConnection, WsConnectionOptions } from './server/ws/WsConnection';
