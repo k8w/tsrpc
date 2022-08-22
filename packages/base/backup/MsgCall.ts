fb09ee01c7d6c117cb2b987192ce5fcd223e1546
k8w <me@k8w.cn>
2022-08-22 12:12:07 +0800

    rm backup

--- packages/base/backup/MsgCall.ts
+++
@@ -1,7 +0,0 @@
-import { BaseConnection } from "./BaseConnection";
-
-export interface MsgCall<MsgName extends keyof Conn['ServiceType']['msg'], Conn extends BaseConnection> {
-    msg: Conn['ServiceType']['msg'][MsgName],
-    msgName: MsgName,
-    conn: Conn
-}
