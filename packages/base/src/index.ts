ad4e4a7da997e3830c06c2b4f17d05a1a979e6f7
k8w <me@k8w.cn>
2022-09-14 16:14:06 +0800

    tsrpc-proto

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -22,19 +22,3 @@ export * from './proto/ServiceProto';
 export * from './proto/TransportDataProto';
 export * from './proto/TransportDataSchema';
 export * from './proto/TsrpcError';
-
-/** @deprecated Back compatibility for 'tsrpc-proto' */
-declare module 'tsrpc-proto' {
-    /** @deprecated Import from 'tsbuffer-schema' instead */
-    export type int = tsbuffer_schema.int;
-    /** @deprecated Import from 'tsbuffer-schema' instead */
-    export type uint = tsbuffer_schema.uint;
-    /** @deprecated Import from 'tsbuffer-schema' instead */
-    export type double = tsbuffer_schema.double;
-    /** @deprecated Import from 'tsbuffer-schema' instead */
-    export type bigint64 = tsbuffer_schema.bigint64;
-    /** @deprecated Import from 'tsbuffer-schema' instead */
-    export type biguint64 = tsbuffer_schema.biguint64;
-    /** @deprecated Import from 'tsbuffer-schema' instead */
-    export type Overwrite<T, U> = tsbuffer_schema.Overwrite<T, U>;
-}
---
+++ packages/proto/index.d.ts
@@ -0,0 +1,14 @@
+import * as tsbuffer_schema from 'tsbuffer-schema';
+
+/** @deprecated Import from 'tsbuffer-schema' instead */
+export type int = tsbuffer_schema.int;
+/** @deprecated Import from 'tsbuffer-schema' instead */
+export type uint = tsbuffer_schema.uint;
+/** @deprecated Import from 'tsbuffer-schema' instead */
+export type double = tsbuffer_schema.double;
+/** @deprecated Import from 'tsbuffer-schema' instead */
+export type bigint64 = tsbuffer_schema.bigint64;
+/** @deprecated Import from 'tsbuffer-schema' instead */
+export type biguint64 = tsbuffer_schema.biguint64;
+/** @deprecated Import from 'tsbuffer-schema' instead */
+export type Overwrite<T, U> = tsbuffer_schema.Overwrite<T, U>;
