8bde1169054ab9faf58429f04cf2397255e1f501
k8w <me@k8w.cn>
2022-11-22 00:58:29 +0800

    chore: adapt new tsrpc-base

--- packages/base-client/src/base/BaseClient.ts
+++ packages/base-client/src/base/BaseClient.ts
@@ -1,5 +1,5 @@
 import { TSBuffer } from "tsbuffer";
-import { BaseConnection, BaseConnectionDataType, BaseConnectionOptions, BaseServiceType, defaultBaseConnectionOptions, Flow, getCustomObjectIdTypes, LogLevel, ProtoInfo, ServiceMapUtil, ServiceProto, setLogLevel } from "tsrpc-base";
+import { ApiHandler, ApiHandlerUtil, AutoImplementApiReturn, BaseConnection, BaseConnectionDataType, BaseConnectionOptions, BaseServiceType, defaultBaseConnectionOptions, Flow, getCustomObjectIdTypes, LocalApiName, LogLevel, ProtoInfo, SendDataFlow, ServiceMapUtil, ServiceProto, setLogLevel } from "tsrpc-base";
 import { BaseClientFlows } from "./BaseClientFlows";
 
 /**
@@ -17,6 +17,8 @@ import { BaseClientFlows } from "./BaseClientFlows";
  * {@link https://github.com/k8w/tsrpc-miniapp}
  */
 export abstract class BaseClient<ServiceType extends BaseServiceType = any> extends BaseConnection<ServiceType> {
+
+    declare side: 'client';
 
     declare readonly options: BaseClientOptions;
 
@@ -36,7 +38,7 @@ export abstract class BaseClient<ServiceType extends BaseServiceType = any> exte
     };
 
     constructor(serviceProto: ServiceProto<ServiceType>, options: BaseClientOptions, privateOptions: PrivateBaseClientOptions) {
-        const serviceMap = ServiceMapUtil.getServiceMap(serviceProto);
+        const serviceMap = ServiceMapUtil.getServiceMap(serviceProto, 'client');
         const tsbuffer = new TSBuffer({
             ...serviceProto.types,
             ...getCustomObjectIdTypes(privateOptions.classObjectId)
@@ -59,6 +61,34 @@ export abstract class BaseClient<ServiceType extends BaseServiceType = any> exte
 
     // TODO base connect
     // Some 不需要连接（如 HTTP IPC），也可以视为自动连接（屏蔽 connect 方法）
+
+    /**
+     * Register an implementation function for a client-side API.
+     * So that when `ApiCall` is receiving, it can be handled correctly.
+     * @param apiName
+     * @param handler
+     */
+    implementApi<T extends LocalApiName<this>>(apiName: T, handler: ApiHandler<this, T>): void {
+        return ApiHandlerUtil.implementApi(this, this._apiHandlers, apiName, handler);
+    };
+
+    /**
+     * Implement all apis from `apiDir` automatically
+     * @param apiDir The same structure with protocols folder, each `PtlXXX.ts` has a corresponding `ApiXXX.ts`
+     * @param delay Delay or maxDelayTime(ms), `true` means no maxDelayTime (delay to when the api is called).
+     */
+    async autoImplementApi(apiDir: string, delay?: boolean | number): Promise<AutoImplementApiReturn>;
+    /**
+     * Implement single api or a group of api from `apiDir` automatically
+     * You can end with a wildchard `*` to match a group of APIs, like `autoImplementApi('user/*', 'src/api/user')`.
+     * @param apiName The name of API to implement. 
+     * @param apiDir The same structure with protocols folder, each `PtlXXX.ts` has a corresponding `ApiXXX.ts`
+     * @param delay Delay or maxDelayTime(ms), `true` means no maxDelayTime (delay to when the api is called).
+     */
+    async autoImplementApi(apiName: string, apiDir: string, delay?: boolean | number): Promise<AutoImplementApiReturn>;
+    async autoImplementApi(dirOrName: string, dirOrDelay?: string | boolean | number, delay?: boolean | number): Promise<AutoImplementApiReturn> {
+        return ApiHandlerUtil.autoImplementApi(this, this._apiHandlers, dirOrName, dirOrDelay, delay)
+    }
 
     // #region Deprecated 3.x API
     /** @deprecated Use `this.options.dataType` instead. */
--- packages/base-client/src/base/BaseClientFlows.ts
+++ packages/base-client/src/base/BaseClientFlows.ts
@@ -1,7 +1,7 @@
 import { BaseConnectionFlows, Flow, OpResultVoid } from "tsrpc-base";
 import { BaseClient } from "./BaseClient";
 
-export type BaseClientFlows<Conn extends BaseClient> = BaseConnectionFlows<Conn, Conn['ServiceType']> & {
+export type BaseClientFlows<Conn extends BaseClient> = BaseConnectionFlows<Conn> & {
     preConnectFlow: Flow<{
         readonly conn: Conn,
         /** Return `res` to `client.connect()`, without latter connect procedure */
