3b99b8a72a78513c626db1d0a0847bb8e8b6eba4
k8w <me@k8w.cn>
2022-11-24 22:08:02 +0800

    chore: add tsrpc/test

---
+++ packages/tsrpc/test/Base.ts
@@ -0,0 +1,1 @@
+import 'k8w-extend-native';
---
+++ packages/tsrpc/test/proto/PtlObjId.ts
@@ -0,0 +1,14 @@
+// @ts-ignore
+import { ObjectId } from "mongodb";
+
+export interface ReqObjId {
+    id1: ObjectId;
+    buf?: Uint8Array,
+    date?: Date
+}
+
+export interface ResObjId {
+    id2: ObjectId;
+    buf?: Uint8Array,
+    date?: Date
+}
